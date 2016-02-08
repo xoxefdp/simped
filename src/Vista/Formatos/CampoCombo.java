00ca034be798f66b31bb090f16721de97acc410d
José Diaz <xoxefdp@gmail.com>
2016-02-08 18:06:04 -0430

    Creada clase formato para campos combo

---
+++ src/Vista/Formatos/CampoCombo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista.Formatos;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author josediaz
+ */
+public class CampoCombo extends JPanel{
+    
+    private final JLabel etiqueta;
+    private final JComboBox campocombo;
+    
+    /**
+     * Crea un panel con título en el borde, con un campo de texto dentro de
+     * @param nombre nombre a colocar en el borde del campo
+     * @param elementos elemntos que se agregaran como opciones al campo combo
+     */
+    public CampoCombo(String nombre, String [] elementos){
+
+        etiqueta = new JLabel(nombre);
+        campocombo = new JComboBox(elementos);
+        add(etiqueta);
+        add(campocombo);
+    }
+    
+    /**
+     * Devuelve un objeto con el contenido del campo combo seleccionado.
+     * @return 
+     */
+    public Object obtenerSeleccion(){
+        return campocombo.getSelectedItem();
+    }
+}
